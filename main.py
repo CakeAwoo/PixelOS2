5d0ab53cec05c72115faec9080fbf14c9511e528
CakeAwoo <52306737+CakeAwoo@users.noreply.github.com>
2019-12-07 14:27:53 -0500

    changed some bad

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def bootscreen():
 
 @app.route('/desktop/')
 
-def boot():
+def homescreen():
   
 
 	data = '<title>PixelOS</title>  <style>body { background-image: url("http://localhost:8000/wallpaper.jpg");   -webkit-background-size: cover;  -moz-background-size: cover;  -o-background-size: cover;  background-size: cover;  margin: 0;  font-family: Arial, Helvetica, sans-serif;}.navbar {  overflow: hidden;  background-color: #333;  position: fixed;  bottom: 0;  width: 100%;}.navbar a {  float: left;  display: block;  color: #f2f2f2;  text-align: center;  padding: 14px 16px;  text-decoration: none;  font-size: 17px;}.navbar a:hover {  background: #f1f1f1;  color: black;}.navbar a.active {  background-color: #4CAF50;  color: white;}.main {  padding: 16px;  margin-bottom: 30px;}</style></head><body><div class="navbar">  <a href="#home" class="active">Start</a></div>'
