84f2c8aebcd7823b938a130649741def4dba80c2
Cake <jellobot9000@gmail.com>
2019-12-16 17:46:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ def bootscreen():
 def homescreen():
   
 
-	data = '<title>PixelOS</title>  <style>body { background-image: url("http://localhost:8000/wallpaper.jpg");   -webkit-background-size: cover;  -moz-background-size: cover;  -o-background-size: cover;  background-size: cover;  margin: 0;  font-family: Arial, Helvetica, sans-serif;}.navbar {  overflow: hidden;  background-color: #333;  position: fixed;  bottom: 0;  width: 100%;}.navbar a {  float: left;  display: block;  color: #f2f2f2;  text-align: center;  padding: 14px 16px;  text-decoration: none;  font-size: 17px;}.navbar a:hover {  background: #f1f1f1;  color: black;}.navbar a.active {  background-color: #4CAF50;  color: white;}.main {  padding: 16px;  margin-bottom: 30px;}</style></head><body><div class="navbar">  <a href="#home" class="active">Start</a></div>'
+	data = '<title>PixelOS</title>  <style> body { background-image: url("https://cdn.discordapp.com/attachments/601626775554097162/656038768037593130/unknown.png");   -webkit-background-size: cover;  -moz-background-size: cover;  -o-background-size: cover;  background-size: cover;  margin: 0;  font-family: Arial, Helvetica, sans-serif;}.navbar {  overflow: hidden;  background-color: #235edc;  position: fixed;  bottom: 0;  width: 100%;}.navbar a {  float: left;  display: block;  color: #235edc;  text-align: center;  padding: 14px 16px;  text-decoration: none;  font-size: 17px;}.navbar a:hover {  background: #f1f1f1;  color: black;}.navbar a.active {  background-color: #3C983E;  color: white;}.main {  padding: 16px;  margin-bottom: 30px;}</style></head><body><div class="navbar">  <a href="#home" class="active">Start</a></div>'
 
 	return (data)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
